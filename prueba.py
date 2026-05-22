a0947b8daef62c82ee9c6fa3c814fdbae9a95c8e
Xdennizhu20X <dennislrj25@gmail.com>
2026-05-21 19:55:07 -0500

    Trabajo en Clase Dennis Rodríguez

---
+++ prueba.py
@@ -0,0 +1,15 @@
+import paho.mqtt.client as mqtt
+
+client = mqtt.Client(mqtt.CallbackAPIVersion.VERSION2)
+
+client.connect("localhost", 1883, 60)
+
+client.loop_start()
+
+client.publish("uide/lab/temperaturas", "22.22")
+
+client.disconnect()
+
+print("Mensaje enviado")
+
+
